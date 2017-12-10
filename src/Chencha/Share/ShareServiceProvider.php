7a808f6fe50213d51f047635872fe0eb68ad3692
Tim Joosten <Tjoosten3@gmail.com>
2017-12-10 02:26:51 +0100

    Fix nitpicks

--- src/Chencha/Share/ShareServiceProvider.php
+++ src/Chencha/Share/ShareServiceProvider.php
@@ -2,51 +2,50 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class ShareServiceProvider extends ServiceProvider {
+class ShareServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
         $configPath = __DIR__.'/../../config/social-share.php';
         $this->publishes([$configPath => config_path('social-share.php')]);
         $this->loadViewsFrom(__DIR__.'/../../views', 'social-share');
-	}
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $configPath = __DIR__.'/../../config/social-share.php';
         $this->mergeConfigFrom($configPath, 'social-share');
 
-		$this->app->singleton('share', function($app)
-		{
-			return new Share($app);
-		});
-	}
+        $this->app->singleton('share', function ($app) {
+            return new Share($app);
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('share');
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['share'];
+    }
 }
